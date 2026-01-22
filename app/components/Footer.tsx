6b8e13da13e965c38d7a307951ec52f0740f7b85
Gilang PC Kantor <gilang.id@yahoo.com>
2026-01-22 14:34:08 +0700

    update 22 januari edit footer + floorplan

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -34,27 +34,6 @@ export default function Footer() {
                       <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
                     </svg>
                 </a>
-                <a
-                  href="#"
-                  className="w-8 h-8 rounded-full border border-neutral-200 flex items-center justify-center hover:border-neutral-400 hover:text-neutral-900 transition-colors"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                    role="img"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    data-icon="simple-icons:github"
-                    className=""
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-                      className=""
-                    />
-                  </svg>
-                </a>
               </div>
               
               <div className="flex items-center justify-start gap-4 text-neutral-400 mt-10">
@@ -79,7 +58,7 @@ export default function Footer() {
                       href="#"
                       className="hover:text-neutral-900 transition-colors"
                     >
-                      Cerita Kami
+                      Tentang Kami
                     </a>
                   </li>
                   <li className="">
@@ -98,14 +77,6 @@ export default function Footer() {
                       Fasilitas
                     </a>
                   </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="hover:text-neutral-900 transition-colors"
-                    >
-                      Tenan
-                    </a>
-                  </li>
                 </ul>
               </div>
               <div className="space-y-3">
@@ -118,7 +89,7 @@ export default function Footer() {
                       href="#"
                       className="hover:text-neutral-900 transition-colors"
                     >
-                      Berita
+                      Artikel
                     </a>
                   </li>
                   <li>
