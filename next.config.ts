344a3903103528a1a61abd70b3e19be133be79aa
Gilang PC Kantor <gilang.id@yahoo.com>
2026-01-28 09:16:19 +0700

    final commit

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "hoirqrkdgbmvpwutwuwj.supabase.co",
-        port: "",
-        pathname: "/storage/v1/object/public/assets/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
   /* config options here */
 };
 
